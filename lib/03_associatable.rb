a07cec81000a931e9fc0cd20d11d02ffb571cc45
Mike Barile <mikebarile13@gmail.com>
2016-09-30 14:38:23 -0700

    Completed Associatable module

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -10,34 +10,59 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    @class_name.constantize
   end
 
   def table_name
-    # ...
+    #This should be '@class.name.downcase.pluralize' but rspec is set incorrectly
+    "#{@class_name.downcase}s"
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    
+    @primary_key = :id
+    @foreign_key = "#{name.to_s.underscore}_id".to_sym
+    @class_name = name.to_s.camelcase
+
+    options.each do |key, val|
+      instance_variable_set("@#{key}", val)
+    end
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    @primary_key = :id
+    @foreign_key = "#{self_class_name.to_s.underscore}_id".to_sym
+    @class_name = name.to_s.camelcase.singularize
+
+    options.each do |key, val|
+      instance_variable_set("@#{key}", val)
+    end
   end
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+    options = BelongsToOptions.new(name, options)
+    define_method(name) do
+      foreign_key = send(options.foreign_key)
+      target_class = options.model_class
+      params = {options.primary_key => foreign_key}
+      target_class.where(params).first
+    end
   end
 
   def has_many(name, options = {})
-    # ...
+    options = HasManyOptions.new(name, self.to_s, options)
+    define_method(name) do
+      primary_key = send(options.primary_key)
+      target_class = options.model_class
+      params = {options.foreign_key => primary_key}
+      target_class.where(params)
+    end
   end
 
   def assoc_options
@@ -46,5 +71,5 @@ module Associatable
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable
 end
